9f1573c78fe9f0580586ac4bbc1e302ccce69dbc
Aasish <aasish3187@github.com>
2026-07-23 19:51:53 +0530

    fix: update CORS allowed origins to support deployed Vercel domains

--- onboardbot_v2/app/main.py
+++ onboardbot_v2/app/main.py
@@ -17,7 +17,9 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:8000",
-        "http://127.0.0.1:8000"
+        "http://127.0.0.1:8000",
+        "https://on-boarding-bot.vercel.app",
+        "https://on-boarding-bot-aasish3187-2151s-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
